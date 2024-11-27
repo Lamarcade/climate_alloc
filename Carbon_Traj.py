fe077a1b72f3ae60d5388899ef1cc9adca67940c
Lamarcade <88083042+Lamarcade@users.noreply.github.com>
2024-11-27 15:26:32 +0100

    function to plot company trajectories with the mean

--- Carbon_Traj.py
+++ Carbon_Traj.py
@@ -32,27 +32,87 @@ sectors = indicators.copy()
 sectors.drop(sectors.columns[0], axis = 1, inplace = True)
 sectors = sectors.groupby(by= "GICS Sector Name").mean()
 
-sectors_rate = sectors[sectors.columns[:14]]
-sectors_rate = sectors_rate.reset_index()
 
 #%% Dense format to plot
-def dense_format(df):
-    df_dense = df.melt(id_vars="GICS Sector Name", 
+def dense_format(df, value_name, is_sector = True):
+    
+    id_vars = "GICS Sector Name" if is_sector else "Instrument"
+    df_dense = df.melt(id_vars=id_vars, 
                   var_name="Year", 
-                  value_name="Intensity")
+                  value_name=value_name)
 
     df_dense["Year"] = df_dense["Year"].str.extract(r'Y-(\d+)').astype(int)
     df_dense = df_dense.sort_values(by="Year", ascending = False)
     df_dense["Year"] *= -1
     return df_dense
 
-dense_rate = dense_format(sectors_rate)
+
+sectors_rate = sectors[sectors.columns[:14]]
+sectors_rate = sectors_rate.reset_index()
+dense_rate = dense_format(sectors_rate, "Rate")
 
 sectors_intensity = sectors[sectors.columns[14:]]
 sectors_intensity = sectors_intensity.reset_index()
-dense_intensity = dense_format(sectors_intensity)
+dense_intensity = dense_format(sectors_intensity, "Intensity")
+
+#%% Sample sector and companies
+def select_sectors(df, sector):
+    new_df = df.copy()
+    new_df = new_df[new_df["GICS Sector Name"] == sector]
+    mean_row = new_df[new_df.columns[2:]].mean(axis = 0)
+    mean_row['Instrument'] = 'Mean'
+    mean_row['GICS Sector Name'] = 'Consumer Staples'
+    new_df = pd.concat([new_df, mean_row.to_frame().T], ignore_index=True)
+    return new_df
+
+def rate_or_int(df, rate = True):
+    if rate:
+        new_df = pd.concat([df["Instrument"], df[df.columns[2:16]]], axis = 1)
+        #new_df = new_df.reset_index()
+        dense_df = dense_format(new_df, "Rate", is_sector = False)
+    else:
+        new_df = pd.concat([df["Instrument"], df[df.columns[16:]]], axis = 1)
+        #new_df = new_df.reset_index()
+        dense_df = dense_format(new_df, "Intensity", is_sector = False)
+    return dense_df
+
+df_cs = select_sectors(indicators, "Consumer Staples")
+df_en = select_sectors(indicators, "Energy")
+df_fi = select_sectors(indicators, "Financials")
+
+rate_cs = rate_or_int(df_cs, True)
+int_cs = rate_or_int(df_cs, False)
+
+rate_en = rate_or_int(df_en, True)
+int_en = rate_or_int(df_en, False)
+
+rate_fi = rate_or_int(df_fi, True)
+int_fi = rate_or_int(df_fi, False)
 
 #%% Plots
+
+def history_plot(df, y, is_sector = False, title = "", save = False, filename = None):
+    plt.figure(figsize=(12, 6))
+    
+    if is_sector:
+        sns.lineplot(data=df, x="Year", y=y, hue="GICS Sector Name", marker="o", legend = False)
+    else:
+        df_no_mean = df[df['Instrument'] != 'Mean']  
+        df_mean = df[df['Instrument'] == 'Mean']   
+        sns.lineplot(data=df_mean, x='Year', y=y, color='red', marker='*', linestyle = "", label='Mean', markersize=15)
+        sns.lineplot(data=df_no_mean, x='Year', y=y, color='blue', hue = "Instrument", 
+                     legend = False,linewidth=0.8, alpha=0.4)
+
+    plt.title(title, fontsize=14)
+    plt.xlabel("Year", fontsize=12)
+    plt.ylabel(y, fontsize=12)
+    if is_sector:
+        plt.legend(title="GICS sector", bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.grid(True)
+    plt.tight_layout()
+    if save:
+        plt.savefig(f"Figs/{filename}.png")
+
 def sector_rate(save = False):
     plt.figure(figsize=(12, 6))
     sns.lineplot(data=dense_rate, 
@@ -84,9 +144,10 @@ def sector_intensity(save = False):
     plt.legend(title="GICS sector", bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig("Figs/intensity.png")
+    if save:
+        plt.savefig("Figs/intensity.png")
 
-def count_sectors():
+def count_sectors(save):
     sector_counts = indicators["GICS Sector Name"].value_counts().reset_index()
     sector_counts.columns = ["GICS Sector Name", "Number of Companies"]
     
@@ -103,4 +164,15 @@ def count_sectors():
     plt.tight_layout()
     
     plt.show()
-    #plt.savefig("Figs/sectors.png")
+    if save:
+        plt.savefig("Figs/sectors.png")
+        
+#%% 
+history_plot(rate_cs, "Rate", is_sector = False, title = "Consumer staples companies carbon rate evolution", save = True, filename = "ratecs")
+history_plot(int_cs, "Intensity", is_sector = False, title = "Consumer staples companies carbon intensity evolution", save = True, filename = "intcs")
+
+history_plot(rate_en, "Rate", is_sector = False, title = "Energy companies carbon rate evolution", save = True, filename = "rateen")
+history_plot(int_en, "Intensity", is_sector = False, title = "Energy companies carbon intensity evolution", save = True, filename = "inten")
+
+history_plot(rate_fi, "Rate", is_sector = False, title = "Financial companies carbon rate evolution", save = True, filename = "ratefi")
+history_plot(int_fi, "Intensity", is_sector = False, title = "Financial companies carbon intensity evolution", save = True, filename = "intfi")
