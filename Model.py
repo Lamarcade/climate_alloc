3360faeaafaa4dd3069ae373205b4a927d0b0e8f
Lamarcade <88083042+Lamarcade@users.noreply.github.com>
2025-01-15 15:15:19 +0100

    model updated and params for testing

--- Model.py
+++ Model.py
@@ -25,7 +25,7 @@ from scipy.stats import multivariate_normal
 class Model():  
     
 #%% Initialization
-    def __init__(self, Time, initial_law = [0.25, 0.25, 0.25, 0.25], data_file = "Data/Stoxx_data.xlsx"):
+    def __init__(self, Time, initial_law = np.array([0.25, 0.25, 0.25, 0.25])[:, np.newaxis], data_file = "Data/Stoxx_data.xlsx"):
         '''
         Initialize an instance of the model computing the filter and performing the EM algorithm
 
@@ -65,13 +65,29 @@ class Model():
         indicators = df[["Instrument","GICS Sector Name"]].copy()
         for i in range(13, -1, -1):
             # Actual year / Former year from Y-13 to Y-0
-            indicators["Rate Y-{i}".format(i = i)] = 100 * df["Total Y-{i}".format(i = i)] / df["Total Y-{j}".format(j = i+1)]
+            #indicators["Rate Y-{i}".format(i = i)] = 100 * df["Total Y-{i}".format(i = i)] / df["Total Y-{j}".format(j = i+1)]
+            
+            # Keep as a percentage
+            indicators["Rate Y-{i}".format(i = i)] = df["Total Y-{i}".format(i = i)] / df["Total Y-{j}".format(j = i+1)]
         
+        # Reduce number of rates and drop NaN
         indicators.replace([np.inf, -np.inf], np.nan, inplace = True)
+        ind = indicators.dropna()
+        ind = ind.iloc[:6]
+        
+        # Use sector decarbonation rates
+        sectors = indicators.copy()
+        sectors.drop(sectors.columns[0], axis = 1, inplace = True)
+        sectors = sectors.groupby(by= "GICS Sector Name").mean()
+        sectors.drop("Real Estate", inplace = True)
+        
         #for i in range(13, -1, -1):
         #    # Total emissions / Revenue 
         #    indicators["Intensity Y-{i}".format(i = i)] = df["Total Y-{i}".format(i = i)] / df["Revenue Y-{i}".format(i = i)]
-        self.indicators = indicators
+        
+        #self.indicators = ind
+        self.indicators = sectors
+        
         
         # Historical central carbon rate
         self.mus = np.zeros((len(initial_law),Time))
@@ -79,10 +95,10 @@ class Model():
         mu = 0
         
         # Duration of the historical data
-        self.T0 = indicators.shape[1] - 2
+        self.T0 = self.indicators.shape[1] - 2
         
         for t in range(2, self.T0 + 2):
-            mu += indicators.loc[:, indicators.columns[t]].mean(skipna = True)
+            mu += self.indicators.loc[:, indicators.columns[t]].mean(skipna = True)
         mu /= self.T0
         for t in range(self.T0):
             self.mus[:, t] = mu * np.ones(len(initial_law))   
@@ -108,12 +124,13 @@ class Model():
         None.
 
         '''
-        self.theta = (central_std, beta, nus, sigmas)
+        self.theta = np.concatenate([np.array([central_std, beta]), nus, sigmas])
+        self.theta = self.theta.flatten()
         assert len(nus) == len(sigmas), "Mismatch in dimensions of nus and sigmas"
         # 0 : Central std
         # 1 : Beta
-        # 2 : Nus
-        # 3 : Sigmas
+        # 2:2+n (excluded) : Nus
+        # 2+n:2+2n : Sigmas
 
 
 #%% Evaluation functions    
@@ -185,9 +202,23 @@ class Model():
         '''
         # f_t((d_i)i | Delta = j, F_{t-1})
         cov = theta[0] + theta[1] * (previous_intensity - self.mus[scenar, t])**2
+        #print(cov)
+        
+        raise Exception("Function not up to date")
+        
         matrix = cov * np.ones((len(theta[3]), len(theta[3])))
         matrix += np.diag(np.array([cov]) + theta[3])
+        #print(matrix)
+        
+        print("Mean MN " , self.mus[scenar, t] + theta[2])
+        print("Cov Matrix", matrix)
+        print()
+        print("Intensities", intensities)
+        #wait = input("Press Enter")
+        
         mn = multivariate_normal(self.mus[scenar, t] + theta[2], matrix)
+        #print("Intensities", intensities)
+        print("PDF scenario {i}: ".format(i = scenar), mn.pdf(intensities))
         return(mn.pdf(intensities))
  
     def explicit_density(self, theta, intensities, previous_intensity, scenar, t, is_log = False):
@@ -217,26 +248,34 @@ class Model():
         '''
         
         cov = theta[0] + theta[1] * (previous_intensity - self.mus[scenar, t])**2
-        n = len(theta[3])
+        #print("cov", cov)
+        n = len(intensities)
         
         # cov_inverse
-        diago = np.diag( np.array([cov]) / theta[3])
+        diago = np.diag( np.array([cov]) / theta[2+n:])
         #denom = (1 + np.ones(n).dot(diago).dot(np.ones(n)))
         # Simplified formula
-        denom = 1 + cov * np.sum(1/theta[3])
+        denom = 1 + cov * np.sum(1/theta[2+n:])
             
         inverse = 1/ (cov) * (diago - diago.dot(np.ones((n, n))).dot(diago) / denom)
     
         # determinant
         # cov**n * det(D_{j,t}) * denom
-        det = np.prod(theta[3]) * denom
+        det = np.prod(theta[2+n:2+2*n]) * denom
         
         # Computing the density
         coeff = 1/ np.sqrt( (2* np.pi) **n * det) 
+        #print("denom", denom)
+        #print("det", det)
         
         # mu is a single value
-        vector = (intensities - (self.mus[scenar, t] + theta[2]))
+        vector = (intensities - (self.mus[scenar, t] + theta[2:2+n]))
+        #print("Vector", vector)
+        #print("Inverse", inverse)
         inside = -1/2 * vector.dot(inverse).dot(vector)
+        #print("Mahalanobis", inside)
+        #print()
+       # wait = input("Press Enter")
         if is_log:
             return(np.log(coeff) + inside)
         else:
@@ -246,7 +285,7 @@ class Model():
 # previous_intensity = self.df.loc[:,self.history_count + 1].mean(axis = 0)
 # If history count is 0 need to add historical data
  
-    def full_density(self, theta, intensities, previous_intensity, t):
+    def full_density(self, theta, intensities, previous_intensity, t, is_log = False):
         '''
         Return a vector with the density values for each scenario
 
@@ -271,9 +310,9 @@ class Model():
         density = np.zeros(len(self.mus))
         for j in range(len(self.mus)):
             # Scenario j
-            density[j] = self.one_density(theta, intensities, previous_intensity, j, t)
+            density[j] = self.explicit_density(theta, intensities, previous_intensity, j, t, is_log)
         self.density = density
-        return(self.density)
+        return(self.density[:, np.newaxis])
 
 #%% Filter
     
@@ -295,9 +334,15 @@ class Model():
         '''
         # 2.12
         density_val = self.full_density(self.theta, intensities, previous_intensity, self.history_count + 2)
+        print("Density val", density_val)
+        print()
         num = np.multiply(self.pi, density_val)
-        marginal = np.ones(self.pi.shape).dot(num)
-        self.history_pi[:, self.history_count] = self.probas
+        marginal = np.ones(self.pi.shape).T.dot(num).item() # since it is a matrix
+        
+        #print("marginal", marginal)
+        
+        
+        self.history_pi[:, self.history_count] = self.probas.flatten()
         self.history_count += 1
         self.history_marginal[self.history_count] = marginal
         self.probas = num/marginal
@@ -327,10 +372,10 @@ class Model():
         '''
         q1 = 0
         for t in range(full_intensities.shape[1] -2):
-            # Parameters theta[0:3] are used in the density here
+            # Parameters central std, betas, nus are used in the density here
             
             # At t = 0 need to use historical
-            q1 += - np.dot(np.log(self.full_density(theta, full_intensities[:,t+2], full_intensities[:,t+1].mean(axis = 0)), t), self.pi)
+            q1 += np.dot(self.full_density(theta, full_intensities.iloc[:,t+2], full_intensities.iloc[:,t+1].mean(axis = 0), t, is_log = True).T, self.pi).item()
         return q1
     
     def q2(self):
@@ -343,7 +388,7 @@ class Model():
             Second term of the log-likelihood.
 
         '''
-        return np.sum(np.log(self.probas), self.pi)
+        return - np.sum(np.log(self.probas) * self.pi)
     
     def log_lk(self, theta, full_intensities):
         '''
@@ -362,8 +407,19 @@ class Model():
             Value of the log-likelihood.
 
         '''
+        print("Q1:", self.q1(theta, full_intensities))
+        print("Q2:", self.q2())
+        print()
         return(self.q1(theta, full_intensities) + self.q2())
-        
+    
+    def constraint_eq(self, theta,n):
+        '''Constraint: theta[n+1] == -sum(theta[2:n+1]). aka sum(nu_i) = 0'''
+        return theta[n+1] + np.sum(theta[2:n+1])
+
+    def constraint_ineq(self, theta, n):
+        '''Constraint: theta[2+n : 2+2n] > 0. aka (sigma_i)i > 0'''
+        return theta[2+n : 2+2*n]
+    
     def M_step(self, full_intensities):
         '''
         Perform a maximization step in the EM algorithm
@@ -378,9 +434,39 @@ class Model():
         None.
 
         '''
-        result = minimize(self.q1, self.theta, args = (full_intensities), method = 'SLSQP')
+        n = len(full_intensities)
+        constraints = [
+            {'type': 'eq', 'fun': self.constraint_eq, 'args': (n,)},
+            #{'type': 'ineq', 'fun': self.constraint_ineq, 'args': (n,)}
+            ]
+        
+        epsilon = 1e-6
+        bounds = [
+        (0, None),                 # theta[0] > 0
+        (epsilon, 1 - epsilon),    # 0 < theta[1] < 1
+    ] + [(None, None)] * n + [(epsilon, None)] * n  # Positivité pour theta[2+n : 2+2n]
+
+        def check_bounds(theta, bounds):
+            for i, (value, (lower, upper)) in enumerate(zip(theta, bounds)):
+                if lower is not None and value < lower:
+                    print(f"Paramètre theta[{i}] = {value} est inférieur à la borne inférieure {lower}")
+                    return False
+                if upper is not None and value > upper:
+                    print(f"Paramètre theta[{i}] = {value} est supérieur à la borne supérieure {upper}")
+                    return False
+            return True
+        
+        if not check_bounds(self.theta, bounds):
+            raise ValueError("Initial theta values are not in the bounds")
+        
+        result = minimize(self.q1, self.theta, args = (full_intensities), constraints = constraints, 
+                          bounds = bounds, method = 'SLSQP', options={'disp': True})
         if result.success:
             self.theta = result.x
+            print("Theta", self.theta)
+            wait = input("Press Enter")
+        else:
+            print("Failure optimization")
         
     def EM(self, full_intensities, n_iter):
         '''
@@ -398,14 +484,27 @@ class Model():
         None.
 
         '''
+        loglk = []
         for l in range(n_iter):
             # E step
             self.history_count = 0
             for t in range(full_intensities.shape[1] - 2):
-                self.filter_step(full_intensities[:,t+2], full_intensities[:,t+1].mean(axis = 0))
+                self.filter_step(full_intensities.iloc[:,t+2], full_intensities.iloc[:,t+1].mean(axis = 0))
             
             # M step
             self.M_step(full_intensities)
+            loglk.append(self.log_lk(self.theta, full_intensities))
+        return loglk
+            
     
-    
+mm = Model(14)
+
+fi = mm.indicators
+p,q = fi.shape
+
+nn = np.ones(p)
+nn[p//2:] -= 2 * np.ones(p - p//2)
+np.sum(nn)
+mm.initialize_parameters(1, 0.5, nn, 1 * np.ones(p))
+mm.EM(mm.indicators, n_iter = 3)    
     
