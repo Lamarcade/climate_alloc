deb15f47135ba9f41dbe77b22251096d38028a8f
Lamarcade <88083042+Lamarcade@users.noreply.github.com>
2025-01-23 15:57:34 +0100

    added explicit dates for historic data, exported simulated data to excel with function for scenarios

--- Data_simul.py
+++ Data_simul.py
@@ -5,9 +5,11 @@ Created on Tue Jan 21 17:24:07 2025
 @author: LoïcMARCADET
 """
 import pandas as pd
-import numpy as np
+import numpy as np, numpy.random
 import matplotlib.pyplot as plt
 import seaborn as sns
+import random as rd
+
 
 #%%
 
@@ -75,10 +77,10 @@ scenario_data["Ratio"] = scenario_data.groupby(['Model', 'Scenario', 'Region', '
 scenario_data.drop({"Model", "Region", "Unit", "Value", "Variable"}, axis = 1, inplace = True)
 
 
-sc = scenario_data.pivot(index=["Scenario"], columns="Year", values="Ratio")
+final_df = scenario_data.pivot(index=["Scenario"], columns="Year", values="Ratio")
 
 #%%
-df_plot = sc.reset_index().melt(id_vars="Scenario", var_name="Year", value_name="Ratio")
+df_plot = final_df.reset_index().melt(id_vars="Scenario", var_name="Year", value_name="Ratio")
 plt.figure(figsize=(10, 6))
 sns.lineplot(data=df_plot, x="Year", y="Ratio", hue="Scenario", marker="o")
 
@@ -91,3 +93,39 @@ plt.show()
 
 #%% Simulate data for sectors
 
+n_sectors = 10 
+
+scenar2index = {"Below 2°C": 0, "Current Policies" : 1, "Delayed transition" : 2,
+               "Fragmented World" : 3, "Low demand": 4, 
+               "Nationally Determined Contributions (NDCs)":5, 
+               "Net Zero 2050":6}
+
+index2scenar = {0 : "Below 2°C", 1: "Current Policies", 2: "Delayed transition",
+               3:"Fragmented World", 4: "Low demand", 
+               5: "Nationally Determined Contributions (NDCs)", 
+               6:"Net Zero 2050"}
+
+
+def scenario_simul(data = final_df, scenar_index = 6, n_sectors = n_sectors):
+    # Number of years
+    etas = np.random.dirichlet(np.ones(n_sectors),size=len(data.columns)).transpose()
+
+    # Sum to 0 instead of 1
+    etas -= 1/ n_sectors
+    
+    # Retrieve average scenario path
+    base_data = data.loc[index2scenar[scenar_index]]
+    
+    paths = pd.DataFrame(np.ones((n_sectors, len(data.columns))), columns = data.columns)
+    for i in range(len(paths)):
+        paths.loc[i] = base_data
+        paths.loc[i] += etas[i,:]
+    return paths
+
+index_used = 6
+paths = scenario_simul()
+
+summed, base = paths.mean(axis = 0), final_df.loc[index2scenar[index_used]]
+print((summed - base).sum())
+
+paths.to_excel("Data/simul.xlsx", sheet_name = index2scenar[index_used])
